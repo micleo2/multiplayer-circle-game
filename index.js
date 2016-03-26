a9e0e91bf7d08a41537ee34ff70b2fdc9623f347
Michael Leon <programmermike1@gmail.com>
2016-03-26 17:26:17 -0500

    Client will render subsequent clients' squares and positions

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var statId = 3;
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
+	console.log("New client registered");
+  /*socket.on('chat message', function(msg){
     io.emit('chat message', msg);
 	console.log("Broadcasting message to all observers");
+  });*/
+  socket.on("newplayer", function(plyr){
+	  console.log("Player added to pool");
+	  plyr.id = statId++;
+	  socket.broadcast.emit("newplayer", plyr);
+  });
+  
+  socket.on("playerUpdate", function(obj){
+	  socket.broadcast.emit("playerUpdate", obj);
+  });
+  
+  socket.on('disconnect', function () {
+	  console.log("Client unregistered");
   });
 });
 
